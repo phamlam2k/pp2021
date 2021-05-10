c2ee3245838ef5eba420528997f1916899bc7ee5
phamlam2k <76936718+phamlam2k@users.noreply.github.com>
2021-05-10 14:33:09 +0700

    Update input.py

--- pw4/input.py
+++ pw4/input.py
@@ -5,19 +5,19 @@ def my_input(stdscr, r, c, prompt_string):
     curses.echo()
     stdscr.addstr(r, c, prompt_string)
     stdscr.refresh()
-    __input = stdscr.getstr(r + 1, c, 20).decode('utf-8')
-    return __input
+    _input = stdscr.getstr(r + 1, c, 20).decode('utf-8')
+    return _input
 
 
 def findCourseName(courses, course_id):
     for course in courses:
         if course.get_id() == course_id:
             return course.get_name()
-    print("Err: Invalid ID")
+   
 
 
 def findCourseCredit(courses, course_id):
     for course in courses:
         if course.get_id() == course_id:
             return course.get_credit()
-    print("Err: Invalid ID")
+    
